89ab40e5f66038f4194178bf553c3f9b63f9379c
Arnab <arnab4srk@gmail.com>
2020-07-28 08:43:24 +0530

    Added type effect

---
+++ Assets/js/type.js
@@ -0,0 +1,10 @@
+const Skills = ["B.Tech Under Grad", "Front-End Web Developer", "Java & Python Programmer", "Student"];
+let index = 0,
+    skillIndex = 0,
+    skill = "";
+! function e() {
+    skillIndex === Skills.length && (skillIndex = 0);
+    let l = (skill = Skills[skillIndex]).slice(0, ++index);
+    document.querySelector("#type-text").textContent = l, 
+    l.length === skill.length ? (skillIndex++, index = 0, setTimeout(e, 1000)) : setTimeout(e, 100)
+}();
