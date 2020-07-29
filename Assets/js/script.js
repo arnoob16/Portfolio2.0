37a9fb200a6da5ebf57d2c144695d645ac98de80
Arnab <arnab4srk@gmail.com>
2020-07-30 04:56:20 +0530

    Hidden the navbar toggle by offset

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
-    $(".nav-toggle").click(function(){
+$(document).ready(function () {
+    $(".nav-toggle").click(function () {
         $(".nav-toggle").toggleClass("active")
         $(".fa-times").toggleClass("active")
         $(".nav-overlay").toggleClass("active")
         $(".nav-menu").toggleClass("visible")
     })
 })
+
+function get() {
+    if(($('#about').offset().top - $(window).scrollTop()) > 0){
+        $(".nav-toggle").hide();
+        $(".nav-overlay").hide();
+    }
+    else{
+        $(".nav-toggle").show();
+        $(".nav-overlay").show();
+    }
+}
+
+get();
+$(window).scroll(get);
