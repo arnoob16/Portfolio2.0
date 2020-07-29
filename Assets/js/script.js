26ddd23e2de402c2c3dbb7087f9b97e958d98092
Arnab <arnab4srk@gmail.com>
2020-07-29 23:21:52 +0530

    animation made smooth

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".nav-toggle").click(function(){
         $(".nav-toggle").toggleClass("active")
-        $(".fa-bars").toggleClass("active")
+        $(".fa-times").toggleClass("active")
     })
 })
