d8fbae08e9072a36778af2f01c515cf7a2c2c16a
Arnab <contact.arnab16@gmail.com>
2020-10-01 12:18:06 +0530

    Added tabbed view for all projects

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
 
 $(document).ready(function(){
     $(".navItem").click(function(){
-        console.log("clicked");
         $(".nav-toggle").toggleClass("active")
         $(".fa-times").toggleClass("active")
         $(".nav-overlay").toggleClass("active")
@@ -36,3 +35,15 @@ function get() {
 
 get();
 $(window).scroll(get);
+
+function changeGallery(galleryID){
+    let tabID = galleryID.toUpperCase();
+    let prevTab = document.querySelector('.selected');
+    prevTab.classList.remove('selected');
+    document.getElementById(tabID).classList.add('selected');
+    let prevGallery = prevTab.getAttribute('id').toLowerCase() + 'Class';
+    document.getElementById(prevGallery).style.display = 'none';
+    galleryID = galleryID + 'Class';
+    // console.log(galleryID)
+    document.getElementById(galleryID).style.display = 'block';
+}
